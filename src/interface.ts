99b772fa2d0a5b3dc27d9bb9f751d6178481fe67
shubhkm <shubham.madheshiya@succcessive.tech>
2020-01-12 14:14:07 +0530

    remove package.lock.json and yarn.lock

--- src/interface.ts
+++ src/interface.ts
@@ -8,23 +8,8 @@ export interface createTodoInterface{
     type: String,
     text: String,
 }
-  // Todo form interface
+
   export interface AddTodoInterface {
     todoHandler: (todo: createTodoInterface) => void;
   }
-  // Todo list interface
-  export interface TodoListInterface {
-    handleTodoUpdate: (event: React.ChangeEvent<HTMLInputElement>, id: string) => void;
-    handleTodoRemove: (id: string) => void;
-    handleTodoComplete: (id: string) => void;
-    handleTodoBlur: (event: React.ChangeEvent<HTMLInputElement>) => void;
-    todos: TodoInterface[]
-  }
-  // Todo item interface
-  export interface TodoItemInterface {
-    handleTodoUpdate: (event: React.ChangeEvent<HTMLInputElement>, id: string) => void;
-    handleTodoRemove: (id: string) => void;
-    handleTodoComplete: (id: string) => void;
-    handleTodoBlur: (event: React.ChangeEvent<HTMLInputElement>) => void;
-    todo: TodoInterface;
-  }
+  
